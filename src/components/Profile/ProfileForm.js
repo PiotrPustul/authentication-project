5252aaa3ad41a6a49d826995543f69b287a148cb
pepe199111 <piotrpustul1@gmail.com>
2021-06-10 12:57:42 +0100

    finished work on simple authentication using firebase

---
+++ src/components/Profile/ProfileForm.js
@@ -0,0 +1,46 @@
+import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import AuthContext from '../../store/auth-context';
+import classes from './ProfileForm.module.css';
+
+const ProfileForm = () => {
+  const history = useHistory();
+  const { token } = useContext(AuthContext);
+  const newPasswordInputRef = useRef();
+
+  const submitNewPasswordHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCZ31cxrhk2lcF-rD-R2-AT3CJJHICydiE', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        history.replace('/');
+      });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitNewPasswordHandler}>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef} />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
